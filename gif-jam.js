9451c89e3c957f5587a74d921688f2fc27aa8f16
Matúš Duchoň <matus.duchon@gmail.com>
2016-03-26 17:12:19 +0100

    fix error handling

--- gif-jam.js
+++ gif-jam.js
@@ -1,36 +1,30 @@
-'use static';
 'use strict';
 
-export default function getStaticImageDataUrl(imageUrl) {
 function getStaticImageDataUrl(imageUrl) {
 	return new Promise((resolve, reject) => {
 		let image = new Image();
 
-		image.crossOrigin="anonymous"
 		image.crossOrigin = "Anonymous";
 		image.src = imageUrl;
 
 		image.onload = () => {
-			let canvas = document.createElement('canvas')
 			let canvas = document.createElement('canvas');
 
 			canvas.width = image.width;
 			canvas.height = image.height;
 
-			let context = canvas.getContext('2d')
 			let context = canvas.getContext('2d');
 
-			context.drawImage(image, 0, 0)
 			context.drawImage(image, 0, 0);
 
-			let staticImageUrl = canvas.toDataURL()
 			let staticImageUrl = canvas.toDataURL();
 
-			resolve(staticImageUrl)
 			resolve(staticImageUrl);
 		}
 
-		img.onerror = reject;
+		image.onerror = (e) => {
+			resolve(imageUrl);
+		};
 	})
 	
 }
